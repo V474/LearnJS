7f5c2e9961b777c03913a815112cc8d532346bfa
V474 <9835799@gmail.com>
2023-05-28 21:13:52 -0700

    4th

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('Я - JavaScript!');
+function myLocalScope() {
+    // Змініть код лише під цим рядком
+  
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Запустіть та перевірте консоль
+  // myVar не визначений за межами myLocalScope
+  console.log('outside myLocalScope', myVar);
