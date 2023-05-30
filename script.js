94f1c1eada14f5026cb1c56412dbd5a3bcbc386d
V474 <9835799@gmail.com>
2023-05-29 19:56:13 -0700

    second half of basic js

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function myLocalScope() {
   // Запустіть та перевірте консоль
   // myVar не визначений за межами myLocalScope
   console.log('outside myLocalScope', myVar);
+  function caseInSwitch(val) {
+    let answer = "";
+    // Змініть код лише під цим рядком
+  
+  
+  
+    // Змініть код лише над цим рядком
+    return answer;
+  }
+  
+  caseInSwitch(1);
